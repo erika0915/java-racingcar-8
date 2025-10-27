2ce425496975ae6af9ec9268a2a1ce9e719bdd9c
erika0915 <soohee2001@gmail.com>
2025-10-27 10:33:11 +0900

    chore: 파일명 수정

--- src/main/java/racingcar/controller/RacingGame.java
+++ src/main/java/racingcar/controller/RacingGame.java
@@ -8,7 +8,7 @@ import java.util.ArrayList;
 import java.util.List;
 import java.util.stream.Collectors;
 
-public class RacingGameController {
+public class RacingGame {
 
     public void run() {
         List<Car> cars = setupCars();
